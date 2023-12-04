accadb6b0363427e39d72a8da90e687ae9a9260f
Subham <chromaticshubham26@gmail.com>
2023-12-05 01:27:39 +0530

    connected to database

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import mongoose from 'mongoose';
+import { DB_NAME } from './constant.js';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB();
