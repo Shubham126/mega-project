816d88a756240305339c9aabe0be93f912ed6c61
Subham <chromaticshubham26@gmail.com>
2023-12-05 15:10:22 +0530

    enhanced utils, index of src and app.js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ dotenv.config({
     path:"./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on the port ${process.env.PORT}`)
+    })
+})
+.catch((error) =>{
+    console.log("MONGO db connection failed !!", error)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,37 @@
+//This is promise approach 
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => {async() => {}}
+// const asyncHandler = (func) => async () => {}
+
+// this is try catch approach 
+
+// const asyncHandler = (fn) = async(req, res, next) => {
+//     try{
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
