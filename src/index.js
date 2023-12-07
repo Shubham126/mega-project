d9c373f69b2c80fe3c2e077822ffb96087dfdb60
Subham <chromaticshubham26@gmail.com>
2023-12-07 22:57:58 +0530

    routes, controllers and error solving

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import mongoose from 'mongoose';
-import { DB_NAME } from './constant.js';
-import connectDB from './db/index.js';
-
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path:"./env"
+    path: './.env'
 })
+
+
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, () =>{
-        console.log(`Server is running on the port ${process.env.PORT}`)
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((error) =>{
-    console.log("MONGO db connection failed !!", error)
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
